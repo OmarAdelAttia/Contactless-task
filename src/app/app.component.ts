90fd8046b5cbb33941ebd1f1d649c05a39db145b
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2024-08-09 21:19:55 +0300

    deleting unneeded components'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { FormComponent } from "./components/form/form.component";
 import { HeaderComponent } from "./components/shared/header/header.component";
-import { FooterComponent } from "./components/shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormComponent, HeaderComponent, FooterComponent],
+  imports: [FormComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
